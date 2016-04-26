7172c00d44a544f865b3803af21a9b24b3519fc6
Taurus Olson <taurusolson@gmail.com>
2016-04-27 00:02:12 +0200

    Add the --status flag

--- pomodoro/cli.py
+++ pomodoro/cli.py
@@ -23,6 +23,7 @@ from daemon import Daemon
 DEFAULT_ALARM_FILE = '/System/Library/Sounds/Submarine.aiff'
 DEFAULT_PID_FILE = '/tmp/pomodoro-daemon.pid'
 DEFAULT_POMODORO_FILE = os.path.expanduser("~/.pomodoro.txt")
+DEFAULT_PLAYER = 'afplay -v 100'
 
 
 # The POMODORO_FILE is written after the execution of the current pomodoro. 
@@ -41,43 +42,68 @@ class PomodoroDaemon(Daemon):
             pomodoro_file=DEFAULT_POMODORO_FILE,
             alarm_file=DEFAULT_ALARM_FILE, verbose=0):
         super(PomodoroDaemon, self).__init__(pid_file, verbose=verbose)
-        self.cur_index = 0
+        self.pid_file      = pid_file
         self.pomodoro_file = os.path.expanduser(pomodoro_file)
-        self.alarm_file = os.path.expanduser(alarm_file)
-        self.cur_duration = PomodoroDaemon.DURATIONS[self.cur_index]
+        self.alarm_file    = os.path.expanduser(alarm_file)
+        self.index         = None
+        self.duration      = None
+        self._status       = None
+        self.read_file()
+
+    @property
+    def status(self):
+        """Return the current status"""
+        work_msg = 'Work for {minute} minutes.'.format
+        short_break_msg = 'Time for a short break ({minute} minutes)'.format
+        long_break_msg = 'You deserve a long break ({minute} minutes)'.format
+
+        if self.duration == 25:
+            cur_status = work_msg(minute=self.duration)
+        elif self.duration == 5:
+            cur_status = short_break_msg(minute=self.duration)
+        elif self.duration == 20:
+            cur_status = long_break_msg(minute=self.duration)
+        return cur_status
+
+    @status.setter
+    def status(self, new_status):
+        """Set the current status"""
+        self._status = new_status
 
     def read_file(self):
         """Read filename if it exists"""
         if os.path.exists(self.pomodoro_file):
             with open(self.pomodoro_file, 'r') as f:
                 data = f.readline()
-                self.cur_index = int(data)
+                self.index = int(data)
         else:
-            self.cur_index = 0
+            self.index = 0
+        self.duration = PomodoroDaemon.DURATIONS[self.index]
 
     def write_file(self):
+        """Write the next index into the pomodoro file"""
         with open(self.pomodoro_file, 'w') as f:
-            next_index = (self.cur_index+1) % len(PomodoroDaemon.DURATIONS)
+            next_index = (self.index+1) % len(PomodoroDaemon.DURATIONS)
             f.write('{next_index}'.format(next_index=next_index))
 
-    def print_msg(self):
-        work_msg = 'Work for {minute} minutes.'.format
-        short_break_msg = 'Time for a short break ({minute} minutes)'.format
-        long_break_msg = 'You deserve a long break ({minute} minutes)'.format
+    def print_status(self):
+        """Print the current status if there is an ongoing pomodoro.
 
-        if self.cur_duration == 25:
-            print work_msg(minute=self.cur_duration)
-        elif self.cur_duration == 5:
-            print short_break_msg(minute=self.cur_duration)
-        elif self.cur_duration == 20:
-            print long_break_msg(minute=self.cur_duration)
+        Otherwise print the next status.
+        """
+        data = {'status': self.status,
+                'cur': self.index + 1,
+                'total': len(PomodoroDaemon.DURATIONS)}
+        if os.path.exists(self.pid_file):
+            print '\nCurrent status: [{cur}/{total}] {status}\n'.format(**data)
+        else:
+            print '\nNext status: [{cur}/{total}] {status}\n'.format(**data)
 
     def run(self):
-        self.read_file()
-        self.cur_duration = PomodoroDaemon.DURATIONS[self.cur_index]
-        self.print_msg()
-        time.sleep(self.cur_duration * 60)
-        os.system('afplay -v 100 {mp3_file}'.format(mp3_file=self.alarm_file))
+        """Run the timer and play a sound when it's finished"""
+        self.print_status()
+        time.sleep(self.duration * 60)
+        os.system('{player} {mp3_file}'.format(player=DEFAULT_PLAYER, mp3_file=self.alarm_file))
         self.write_file()
 
     def reset(self):
@@ -94,6 +120,7 @@ def main():
     parser.add_argument('--stop', action='store_true', help='Stop the timer')
     parser.add_argument('--restart', action='store_true', help='Restart the timer')
     parser.add_argument('--reset', action='store_true', help='Erase all the pomodori')
+    parser.add_argument('--status', action='store_true', help='Display the status')
     args = parser.parse_args()
 
     if args.start:
@@ -104,6 +131,8 @@ def main():
         pomodoro_daemon.restart()
     elif args.reset:
         pomodoro_daemon.reset()
+    elif args.status:
+        pomodoro_daemon.print_status()
     else:
         parser.print_help()
         sys.exit(2)
